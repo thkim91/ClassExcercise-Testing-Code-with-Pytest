1d0fa98ef46615650d9b28945bdf9e7bc54e41b7
iamth <thkim@google.com>
2018-11-01 17:29:56 -0700

    modified code for #1

--- In_class_exercise_1.py
+++ In_class_exercise_1.py
@@ -1,20 +1,22 @@
 def cal_number_of_seconds(time_period, unit):
     unit_list = ['second', 'minute', 'hour', 'day', 'week', 'month', 'year']
-    if unit in unit_list:
-        if unit == 'second':
-            return time_period
-        elif unit == 'minute':
-            return time_period * 60
-        elif unit == 'hour':
-            return time_period * 60 * 60
-        elif unit == 'day':
-            return time_period * 60 * 60 * 24
-        elif unit == 'week':
-            return time_period * 60 * 60 * 24 * 7
-        elif unit == 'month':
-            return time_period * 60 * 60 * 24 * 30
-        elif unit == 'year':
-            return time_period * 60 * 60 * 24 * 365
-
+    if time_perid >= 0:
+        if unit in unit_list:
+            if unit == 'second':
+                return time_period
+            elif unit == 'minute':
+                return time_period * 60
+            elif unit == 'hour':
+                return time_period * 60 * 60
+            elif unit == 'day':
+                return time_period * 60 * 60 * 24
+            elif unit == 'week':
+                return time_period * 60 * 60 * 24 * 7
+            elif unit == 'month':
+                return time_period * 60 * 60 * 24 * 30
+            elif unit == 'year':
+                return time_period * 60 * 60 * 24 * 365
+        else:
+            raise Exception("sorry, we do not accept your unit")
     else:
-        return "sorry, we do not accept your unit"
+        raise Exception("sorry, we do not accept negative value")
