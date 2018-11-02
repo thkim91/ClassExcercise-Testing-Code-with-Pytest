931072ddf76a2ab0b9662477464d335bd5c45135
yuxiao-pan <43254553+yuxiao-pan@users.noreply.github.com>
2018-11-01 17:35:14 -0700

    Add files via upload

---
+++ Number of Second Tests.py
@@ -0,0 +1,25 @@
+import pytest
+def test_year_to_sec():
+    assert calc_number_of_seconds(5, "year") == 4730400000
+
+def test_month_to_sec():
+    assert calc_number_of_seconds(6, "month") == 15552000
+
+def test_week_to_sec():
+    assert calc_number_of_seconds(7, "week") == 4233600
+
+def test_day_to_sec():
+    assert calc_number_of_seconds(8, "day") == 691200
+
+def test_hour_to_sec():
+    assert calc_number_of_seconds(9, "hour") == 32400
+
+def test_minute_to_sec():
+    assert calc_number_of_seconds(10, "mintues") == 600
+
+def test_second_to_sec():
+    assert calc_number_of_seconds(11, "second") == 11
+
+def test_negative_input():
+    calc_number_of_seconds(-11, "second")
+        assert pytest.raises(Exception)
